d6b03f236d6b9232b108bffd4c6f604de8ffb668
TAwosanya <babatunde.awosanya@yahoo.com>
2014-04-25 12:01:17 +0100

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+========================================================
+
+This document describes the files in this repository and what each does.
+
+The file "run_analysis.R" contains codes to read, merge, clean and subset data. It also includes the code for creating the eventual "tidy data" uploaded for submission.
+
+The file "CodeBook.md" contains Variable description for data and the steps taken in the creation of the tidy data set. This file was produced using the R Markdown from R studio and then edited for minor changes, please feel free to check it out. This file is also included in this repo named "CodeBook.Rmd".
---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+# Read the training data
+subjectTrainData <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE, sep="")
+XTrainData <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE, sep="")
+yTrainData <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE, sep="")
+Train <- cbind.data.frame(subjectTrainData, XTrainData, yTrainData)
+
+# Read the test data
+subjectTestData <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE, sep="")
+XTestData <- read.table("UCI HAR Dataset/test/X_test.txt", header=FALSE, sep="")
+yTestData <- read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE, sep="")
+Test <- cbind.data.frame(subjectTestData, XTestData, yTestData)
+
+# This is task 1: Merge the training and the test data sets to create one data set
+myData <- rbind.data.frame(Train, Test)
+
+# This is task 3: Read the features and activity labels and set the data as appropriate
+activityLabels <- read.csv("UCI HAR Dataset/activity_labels.txt", header=FALSE, sep="")
+features <- read.csv("UCI HAR Dataset/features.txt", header=FALSE, sep="")
+featureNames <- c("subject", as.character(features[[2]]), "activity")
+names(myData) <- featureNames
+
+# This is task 2: Extract only the measurements on the mean and standard deviation for each measurement
+myData[ ,563] <- factor(myData[ ,563], labels=as.character(activityLabels[[2]]))
+neededVariables <- sort(c(1,grep("\\<mean()\\>",names(myData)),grep("\\<std()\\>",names(myData)),563))
+myNewData <- myData[ ,neededVariables]
+
+# This is task 4:  label the data set with descriptive variable or feature (column) names
+myNewDataColNames <- gsub("[()]","",names(myNewData))
+myNewDataColNames <- gsub("-","",myNewDataColNames)
+myNewDataColNames <- gsub("mean","Mean",myNewDataColNames)
+myNewDataColNames <- gsub("X",".X",myNewDataColNames)
+myNewDataColNames <- gsub("Y",".Y",myNewDataColNames)
+myNewDataColNames <- gsub("Z",".Z",myNewDataColNames)
+myNewDataColNames <- gsub("std","Std",myNewDataColNames)
+
+names(myNewData) <- myNewDataColNames
+
+# Peek the data
+head(myNewData)
+
+# This is task 5: Create independent tidy data set with the average of each variable for each activity and each subject
+tidyData <- aggregate(myNewData[,2:67],by=list(myNewData[,1], myNewData[,68]),mean)
+names(tidyData) <- c("subject","activity",names(tidyData)[-c(1:2)])
+
+# Peek tidy data set
+tidyData
+
